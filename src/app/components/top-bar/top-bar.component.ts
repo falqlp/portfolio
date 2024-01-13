e73d3d6ed933d1e9a6f9fcd960cd759d26e5ff6e
Popole <falqleopaul@gmail.com>
2024-01-13 18:48:10 +0100

    language

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {TranslateModule} from "@ngx-translate/core";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -8,4 +8,13 @@ import {TranslateModule} from "@ngx-translate/core";
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  constructor(protected translateService: TranslateService) {}
+
+  protected changeLanguage(): void {
+    this.translateService.currentLang;
+    this.translateService.use(
+      this.translateService.currentLang === 'fr-FR' ? 'en-EN' : 'fr-FR'
+    );
+  }
+}
