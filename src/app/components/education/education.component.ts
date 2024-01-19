0ed7ebbe7b20d9984a7e75b1681022a78b4426b1
Popole <falqleopaul@gmail.com>
2024-01-19 13:50:26 +0100

    fin portfolio

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -72,56 +72,30 @@ export class EducationComponent {
   protected iut = {
     subjects: [
       {
-        title: 'Data',
+        title: 'PHYSICS',
         skills: [
-          'RELATIONAL_DATABASES',
-          'NOSQL',
-          'DATA_BACKUP',
-          'DATA_VISUALIZATION',
-          'COMPUTING_AND_PROGRAMMING_TOOLS',
+          'THERMODYNAMICS',
+          'ELECTRICITY_AND_ELECTRONICS',
+          'MECHANICS_AND_MATERIAL_RESISTANCE',
+          'OPTICAL_SYSTEMS',
+          'FLUID_MECHANICS_AND_VACUUM_TECHNOLOGY',
         ],
       },
       {
-        title: 'DEV',
+        title: 'CHEMISTRY',
         skills: [
-          'ALGORITHMS_AND_DATA_STRUCTURES',
-          'LINUX_SYSTEMS_AND_TOOLS',
-          'WEB_TECHNOLOGIES',
-          'OBJECT_ORIENTED_PROGRAMMING',
-          'SCRIPTING_LANGUAGES',
-        ],
-      },
-      {
-        title: 'CAPTURE_TRANSMISSION_ELECTRONICS',
-        skills: [
-          'DATA_CAPTURE_INSTRUMENTATION',
-          'PHYSICS_OF_TRANSMISSIONS',
-          'COMPONENTS_FOR_EXECUTION_ARCHITECTURE',
-          'TELECOMMUNICATIONS_CHAIN',
-          'NETWORK_PRINCIPLES',
+          'ATOMIC_AND_MOLECULAR_STRUCTURES',
+          'REDOX_CHEMICAL_KINETICS',
+          'SPECTROSCOPIC_TECHNOLOGY',
         ],
       },
       {
         title: 'SCIENTIFIC_FUNDAMENTALS',
-        skills: [
-          'MATHEMATICAL_TECHNIQUES',
-          'PROBABILITIES',
-          'STATISTICS',
-          'ADVANCED_STATISTICS',
-        ],
+        skills: ['MATHEMATICS', 'COMPUTER_SCIENCE'],
       },
       {
         title: 'PROFESSIONAL_ENVIRONMENT',
-        skills: [
-          'CREATIVITY_TECHNIQUES',
-          'PROJECT_MANAGEMENT',
-          'MULTIDISCIPLINARY_WORKSHOP',
-          'FINANCIAL_MANAGEMENT',
-          'ENGLISH',
-          'USAGE_DESIGN',
-          'DIGITAL_LEGISLATION',
-          'WRITTEN_EXPRESSION_TECHNIQUES',
-        ],
+        skills: ['ENGLISH', 'EXPRESSION_COMMUNICATION'],
       },
     ],
   };
--- src/app/components/secondary-projects/secondary-projects.component.ts
+++ src/app/components/secondary-projects/secondary-projects.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TechnoLogosComponent } from '../techno-logos/techno-logos.component';
@@ -14,12 +14,12 @@ import { TechnoLogosComponent } from '../techno-logos/techno-logos.component';
     MatCardModule,
     MatTooltipModule,
     TechnoLogosComponent,
+    NgIf,
   ],
   templateUrl: './secondary-projects.component.html',
   styleUrl: './secondary-projects.component.scss',
 })
 export class SecondaryProjectsComponent {
-  protected readonly technologiesIconPath = 'assets/images/technologies/';
   protected d3js = {
     p: ['D3JS_DESC1', 'D3JS_DESC2'],
     technologies: ['HTML', 'CSS', 'JavaScript', 'D3js'],
@@ -34,4 +34,28 @@ export class SecondaryProjectsComponent {
     p: ['HTML_CSS_BDD_DESC1', 'HTML_CSS_BDD_DESC2'],
     technologies: ['HTML', 'CSS', 'PHP', 'MariaDB', 'Docker'],
   };
+
+  protected projects = [
+    {
+      title: 'D3.js',
+      p: ['D3JS_DESC1', 'D3JS_DESC2'],
+      a: 'https://github.com/falqlp/dataviz',
+      img: 'assets/images/d3js/preview.png',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'D3js'],
+    },
+    {
+      title: 'HTML CSS PHP',
+      p: ['HTML_CSS_BDD_DESC1', 'HTML_CSS_BDD_DESC2'],
+      a: 'https://github.com/falqlp/html-css-bdd',
+      img: 'assets/images/html-css-bdd/preview2.png',
+      technologies: ['HTML', 'CSS', 'PHP', 'MariaDB', 'Docker'],
+    },
+    {
+      title: 'Portfolio',
+      p: ['PORTFOLIO_DESC1', 'PORTFOLIO_DESC2'],
+      a: 'https://github.com/falqlp/portfolio',
+      img: 'assets/images/portfolio/portfolio-preview.png',
+      technologies: ['HTML', 'CSS', 'SCSS', 'Angular'],
+    },
+  ];
 }
