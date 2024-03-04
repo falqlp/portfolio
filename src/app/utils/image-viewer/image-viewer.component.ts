b916298580c2f08101a08d699ef6ad05211a9abf
Popole <falqleopaul@gmail.com>
2024-03-04 12:03:52 +0100

    Improve HTML syntax and enhance image viewer
    This commit refines the syntax in index.html for better code readability. It also enhances the image viewer component by moving image ratio calculation from component initialization (ngAfterViewInit) to the image load event, which improves accuracy and performance. Furthermore, additional Angular common functions are imported for potential future use.

--- src/app/utils/image-viewer/image-viewer.component.ts
+++ src/app/utils/image-viewer/image-viewer.component.ts
@@ -1,35 +1,33 @@
-import { AfterViewInit, Component, ElementRef, Input } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import { BigImageViewerService } from '../big-image-viewer/big-image-viewer.service';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-image-viewer',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf, NgOptimizedImage],
   templateUrl: './image-viewer.component.html',
   styleUrl: './image-viewer.component.scss',
 })
-export class ImageViewerComponent implements AfterViewInit {
+export class ImageViewerComponent {
   @Input() public src: string;
   @Input() public alt: string;
 
-  protected maxHeight = false;
+  protected maxHeight: boolean;
 
   constructor(
     protected bigImageViewerService: BigImageViewerService,
     protected elementRef: ElementRef
   ) {}
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      const hwRatioImg =
-        this.elementRef.nativeElement.children[0].naturalHeight /
-        this.elementRef.nativeElement.children[0].naturalWidth;
-      const hwRatioImgDiv =
-        this.elementRef.nativeElement.parentElement.clientHeight /
-        this.elementRef.nativeElement.parentElement.clientWidth;
-      this.maxHeight = hwRatioImg > hwRatioImgDiv;
-    }, 100);
+  protected onImgLoad(): void {
+    const hwRatioImg =
+      this.elementRef.nativeElement.children[0].naturalHeight /
+      this.elementRef.nativeElement.children[0].naturalWidth;
+    const hwRatioImgDiv =
+      this.elementRef.nativeElement.parentElement.clientHeight /
+      this.elementRef.nativeElement.parentElement.clientWidth;
+    this.maxHeight = hwRatioImg > hwRatioImgDiv;
   }
 
   protected click(): void {
